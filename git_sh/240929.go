41b81fa799b9fa1daa529fa27b716d82f69340f2
yowaimono <zhangyanhai278@gmail.com>
2024-09-29 11:44:23 +0800

    Add 240929.go file

---
+++ git_sh/240929.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// GetCurrentDayInfo returns the current date in YYYY-MM-DD format
+func GetCurrentDayInfo() string {
+    return "2024-09-29"
+}
+
+func main() {
+    fmt.Println(GetCurrentDayInfo())
+}
