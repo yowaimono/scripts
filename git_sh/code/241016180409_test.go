aa82a77b5711ae583dcc2a5cef803af80c3c7a86
yowaimono <zhangyanhai278@gmail.com>
2024-10-16 18:04:09 +0800

    add a file, code\241016180409_test.go

---
+++ git_sh/code/241016180409_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// GetDayInfo returns the current date in YYMMDD format qwmqwmzyhzyh
+func GetDayInfo() string {
+    return "241016180409"
+}
+
+func main() {
+    fmt.Println(GetDayInfo())
+}
